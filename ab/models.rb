97b91ca1856509d3ff1395bcb74e645937f07e92
mdms-scott <mdms.scott@gmail.com>
2011-03-31 16:45:48 -0400

    Added additional keys to Person

--- ab/models.rb
+++ ab/models.rb
@@ -1,6 +1,14 @@
 class Person
   include MongoMapper::Document
   
-  key :name, String
+  key :first_name, String
+  key :last_name, String
+  key :company, String
+  key :phone, String
+  key :email, String
+
+  timestamps!
   
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
