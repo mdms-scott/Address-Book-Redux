600e1a5cbdacafb02941275c393c6a78a98363cf
mdms-scott <mdms.scott@gmail.com>
2011-03-31 18:00:30 -0400

    More changes for Heroku

--- app.rb
+++ app.rb
@@ -3,9 +3,11 @@ require 'sinatra'
 require 'mongo_mapper'
 require 'haml'
 
-if ENV['MONGOHQ_URL']
+if ENV['MONGOHQ_HOST']
   puts "Running on MongoHQ" 
-  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
+  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_HOST'], ENV['MONGOHQ_PORT'])
+  MongoMapper.database = ENV['MONGOHQ_DATABASE']
+  MongoMapper.database.authenticate(ENV['MONGOHQ_USER'],ENV['MONGOHQ_PASSWORD'])
 else
   puts "Using local database" 
   MongoMapper.database = 'address_book'
